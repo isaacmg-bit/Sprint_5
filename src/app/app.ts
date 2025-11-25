35050edfc487769c95bb7535d6180aee54e2f5a3
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-11-25 12:58:17 +0100

    feat: first interface created feat: first component created and working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Gallery } from './gallery/gallery';
+import { Image } from './image';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Gallery],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('sprint_5');
+
+  Gallery: Image = { 
+    id: 123,
+    url: 'https://picsum.photos/id/237/300/200',
+    featured: true,
+  }
 }
