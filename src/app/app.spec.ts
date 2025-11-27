7c607a143fc7bb2d2a0f8245d55a4ff67a5f3826
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-11-27 19:41:23 +0100

    feat: added gallery component test suite

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
+  it('should render the app-gallery component', async () => {
+    fixture.detectChanges();
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sprint_5');
+
+    expect(compiled.querySelector('app-gallery')).toBeTruthy();
   });
 });
--- src/app/gallery/gallery.spec.ts
+++ src/app/gallery/gallery.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Gallery } from './gallery';
 
 describe('Gallery', () => {
@@ -8,16 +7,58 @@ describe('Gallery', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Gallery]
-    })
-    .compileComponents();
+      imports: [Gallery],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Gallery);
     component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
-  it('should create', () => {
+  it('should create the Gallery component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the images signal without items before init', async () => {
+    expect(component.images().length).toBe(0);
+  });
+
+  it('should call handleImages once on initialization', async () => {
+    const handleImagesSpy = vi.spyOn(component, 'handleImages');
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(handleImagesSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set the images signal to 32 items after init', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.images().length).toBe(32);
+  });
+
+  it('image i===0 has featured = true', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.images()[0].featured).toBe(true);
+  });
+  it('image i!=0 has featured = false', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.images()[1].featured).toBe(false);
+    expect(component.images()[11].featured).toBe(false);
+    expect(component.images()[31].featured).toBe(false);
+  });
+
+  it('should render 32 <app-image-item> components', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const imageItems = fixture.nativeElement.querySelectorAll('app-image-item');
+
+    expect(imageItems.length).toBe(32);
   });
 });
